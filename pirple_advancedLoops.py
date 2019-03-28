8565654091497cfc96bdaed1d02df4145a440706
mxquel <maxi.quel@gmx.net>
2019-03-28 18:47:41 +0100

    refractored code into separate functions for board setup and printing

--- pirple_advancedLoops.py
+++ pirple_advancedLoops.py
@@ -3,6 +3,9 @@
 # -----------------------------------------------------------------------------------
 '''Create a function that draws a playing board. The number of rows and columns are the same. 
 After drawing the board, your function should return True.
+
+extra: determine the maximum width and height that your terminal and screen can comfortably fit 
+without wrapping. If someone passes a value greater than the maximum, the function should return False
 '''
 
 # markers
@@ -13,16 +16,22 @@ p = ' '
 
 
 def setup_board(matrix=3):
-  # deterimine necessary character spaces for a specific matrix
+  # determine necessary character spaces for a specific matrix
   places = matrix + (matrix - 1)
 
   row = ''.join([(e if c % 2 == 0 else s) for c in range(places)])
   devider = ''.join([(f if c % 2 == 0 else p) for c in range(places)])
-  board = '\n'.join([(row if place % 2 == 0 else devider) for place in range(places)])
+  fields = [(row if place % 2 == 0 else devider) for place in range(places)]
 
-  print(board)
+  return places, fields
 
+
+def print_board(board):
+  places, fields = board
+  
+  print('\n'.join(fields))
+  
   return True
 
 
-setup_board()
+print_board(setup_board())
