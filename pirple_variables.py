d6719659cf8fd29a4e01d4d0e68ae67a0e08773d
mxquel <maxi.quel@gmx.net>
2019-03-28 01:20:32 +0100

    first commit of assigment #1

---
+++ pirple_variables.py
@@ -0,0 +1,26 @@
+# -----------------------------------------------------------------------------------
+# HOMEWORK #1: VARIABLES
+# -----------------------------------------------------------------------------------
+'''create  variables of meta data for a favourite song'''
+
+title = 'Run'
+released = (2004, 1, 26) #tuple of year, month, day
+year_recorded = 2003
+artist = 'Snow Patrol' 
+album = 'Final Straw'
+b_side = 'Post Punk Progression'
+genre = 'Indie'
+labels = ['Fiction Records', 'Polydor'] #list of record lables
+producer = 'Jacknife Lee'
+
+# list of attributed writers
+song_writers = ['Gary Lightbody', 'Jonathan Quinn', 'Mark McClelland', 'Nathan Connolly', 'Iain Archer'] 
+
+# dictionary of studios and respective city
+studios = {'London': 'Britannia Row Studios', 'Glasgow': 'The Divining Bell Longue Studios'}
+
+# dictionary of lenghs for different versions in minutes
+lengths_in_min = {'album': 5.93, 'radio edit': 4.27}
+
+# nested list of tuples of artist(s) and year
+selected_covers = [('Leona Lewis', 2008), ('Sarah Engels', 2011), ('Katharine McPhee', 2012), ('Damian McGinty', 2012), ('Josh Groban and Sarah McLachlan', 2018)]
