13b6baf0a61f60dfb8cb92530396c61f26fa90bd
mxquel <maxi.quel@gmx.net>
2019-03-28 14:51:53 +0100

    modified script to check for prime numbers

--- pirple_basicLoops.py
+++ pirple_basicLoops.py
@@ -8,17 +8,34 @@
 	if the number is a multiple of of both three and five print "FizzBuzz"
 
 	In all other instances print the number
+
+	extra: print 'prime' for any prime number
 '''
+
+def check_prime(num):
+	if num >= 2:
+		for f in range(2, num):
+			if num % f == 0:
+				return False
+		else:
+				return True
+	else:
+		return False
+
+
 
 def print_fizz_buzz(lower, upper):
 	for i in range(lower, upper + 1):
-		if i % 3 == 0 and i % 5 == 0:
-			print('FizzBuzz')
-		elif i % 3 == 0:
-			print('Fizz')
-		elif i % 5 == 0:
-			print('Buzz')
+		if check_prime(i):
+			print('prime')
 		else:
-			print(i)
+			if i % 3 == 0 and i % 5 == 0:
+				print('FizzBuzz')
+			elif i % 3 == 0:
+				print('Fizz')
+			elif i % 5 == 0:
+				print('Buzz')
+			else:
+				print(i)
 
 print_fizz_buzz(1, 100)
