415ef4c6b3ac1ceb3e7b3e41d1894bf896c323f3
mxquel <maxi.quel@gmx.net>
2019-03-28 06:49:26 +0100

    added description to function

--- pirple_lists.py
+++ pirple_lists.py
@@ -1,17 +1,18 @@
 # -----------------------------------------------------------------------------------
 # HOMEWORK #4: LISTS
 # -----------------------------------------------------------------------------------
-'''Create a function that allows you to add things to a list. 
-Anything that's passed to this function should get added to myUniqueList, 
-unless its value already exists in myUniqueList. 
+'''Create a function that allows you to add things to a list.
+Anything that's passed to this function should get added to myUniqueList,
+unless its value already exists in myUniqueList.
 
-(If the value doesn't exist already, it should be added and the function should return True. 
+(If the value doesn't exist already, it should be added and the function should return True.
 If the value does exist, it should not be added, and the function should return False)
 
-Add some code below that tests the function, showcasing the different scenarios, 
+Add some code below that tests the function, showcasing the different scenarios,
 and then finally print the value of myUniqueList
 
-extra: Add another function that pushes all the rejected inputs into a separate global array called myLeftovers. 
+extra: Add another function that pushes all the rejected inputs into a separate global array
+called myLeftovers.
 '''
 
 
@@ -19,6 +20,8 @@ myUniqueList = []
 myLeftovers = []
 
 def compile_unique_list(item):
+	'''depending on uniqueness, allocate item to myUniqueList or myLeftovers'''
+
 	if item not in myUniqueList:
 		myUniqueList.append(item)
 		return True
