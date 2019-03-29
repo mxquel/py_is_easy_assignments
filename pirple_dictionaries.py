7790467e543d74476bc70c196e228f6de8054e3c
mxquel <maxi.quel@gmx.net>
2019-03-29 21:35:11 +0100

    first commit of assignment #7

---
+++ pirple_dictionaries.py
@@ -0,0 +1,34 @@
+# -----------------------------------------------------------------------------------
+# HOMEWORK #7: DICTIONARIES AND SETS
+# -----------------------------------------------------------------------------------
+'''refactor the code from first assigment so every variable and its corresponding value
+become key value pairs in a dictionary. then loop through each item in the dictionary
+to print out the key and value
+
+extra: Create a function that allows someone to guess the value of any key in the dictionary,
+and find out if they were right or wrong.
+This function should accept two parameters: Key and Value. If the key exists
+in the dictionary and that value is the correct value, then the function should return True.
+In all other cases, it should return False
+'''
+
+favorite_song = {
+    'title': 'Run',
+    'released': (2004, 1, 26),
+    'year recorded': 2003,
+    'artist': 'Snow Patrol',
+    'album': 'Final Straw',
+    'b-side': 'Post Punk Progression',
+    'genre': 'Indie',
+    'labels': ['Fiction Records', 'Polydor'],
+    'producer': 'Jacknife Lee',
+    'song writers': ['Gary Lightbody', 'Jonathan Quinn', 'Mark McClelland', 'Nathan Connolly', 'Iain Archer'],
+    'studios': {'London': 'Britannia Row Studios', 'Glasgow': 'The Divining Bell Longue Studios'},
+    'lengths in min': {'album': 5.93, 'radio edit': 4.27}
+}
+
+# print where values are strings:
+[print(f"* {key.title()}: {value}") for key, value in favorite_song.items() if isinstance(value, str)]
+
+# print where values are lists or dictionaries
+[print(f"* {key.title()}: {', '.join(value)}") for key, value in favorite_song.items() if isinstance(value, (list, dict))]
